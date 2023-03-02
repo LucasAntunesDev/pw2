6727a5af2f93c6bfe20b3f6f491a67cc21fb4cb2
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-03-02 11:20:43 -0300

    update

--- Fevereiro/Aula 05/2.php
+++ Fevereiro/Aula 05/2.php
@@ -1,16 +1,93 @@
 <?php
 
-$numero = 10;
+$numero = 5;
+
 for ($i = 0; $i < $numero; $i++) {
-    $vetor[$i] = null;
+    $vetor[$i] = rand(1, 100);
 }
 
-foreach ($vetor as $chave => $valor) {
-    $num = rand(1, 10);
+print_
+// // for ($i = 0; $i < $numero; $i++) {
+// // $vetor_primos[$i] = null;
+// // }
 
-    $vetor[$chave] = $num;
-    echo $vetor[$i];
-}
-echo "<pre>";
+// foreach ($vetor as $chave => $valor) {
+//     //Cria um número aleatório
+//     $num = rand(1, 100);
+//     //Armazena esse número em um array
+//     $vetor[$chave] = $num;
+//     // echo var_dump($vetor[$chave]);
+//     $i = 1;
+//     while($i<=$vetor[$chave]){
+//         // echo "var_dump($vetor[$chave]) <br>";
+//         $i++;
+//     }
 
-echo "</pre>";
+//     // if()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // for ($i = 0; $i < $numero; $i++) {
+// // $vetor_primos[$i] = null;
+// // }
+
+// foreach ($vetor as $chave => $valor) {
+//     $num = rand(1, 10);
+
+//     $vetor[$chave] = $num;
+//     echo "var_dump($vetor[$chave])<br>";
+//     $i = 1;
+//     while ($i < $vetor[$chave]) {
+//         $i++;
+//     }
+//     $primo = $vetor[$chave] % $i;
+//     // echo var_dump($primo);
+//     $vetor_primos[] = '';
+//     $vetor_primo[$chave] = $primo;
+//     // echo var_dump($vetor_primo[$chave]);
+//     // echo "<pre>";
+//     // echo var_dump($vetor_primo[$chave]);
+//     // echo "</pre>";
+//     // echo $vetor[$i];
+// }
