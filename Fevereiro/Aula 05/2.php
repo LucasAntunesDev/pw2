73709d4116a138a4038a044591b49ac5aba9032c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-03-04 19:29:36 -0300

    mais exercícios concluídos

--- Fevereiro/Aula 05/2.php
+++ Fevereiro/Aula 05/2.php
@@ -1,93 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-$numero = 5;
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Números Primos</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
 
-for ($i = 0; $i < $numero; $i++) {
-    $vetor[$i] = rand(1, 100);
-}
+<body>
+        <h1>Números primos</h1>
+    <?php
 
-print_
-// // for ($i = 0; $i < $numero; $i++) {
-// // $vetor_primos[$i] = null;
-// // }
+    // Faça um programa que leia um vetor com n posições para números
+    // inteiros. Crie, a seguir, um vetor resultante que contenha todos os números primos do
+    // vetor digitado. Mostre os valores do vetor resultante.
 
-// foreach ($vetor as $chave => $valor) {
-//     //Cria um número aleatório
-//     $num = rand(1, 100);
-//     //Armazena esse número em um array
-//     $vetor[$chave] = $num;
-//     // echo var_dump($vetor[$chave]);
-//     $i = 1;
-//     while($i<=$vetor[$chave]){
-//         // echo "var_dump($vetor[$chave]) <br>";
-//         $i++;
-//     }
+    $numero = 5;
 
-//     // if()
-// }
+    for ($i = 0; $i < $numero; $i++) {
+        $vetor[$i] = rand(1, 100);
+    }
 
+    foreach ($vetor as $chave => $valor) {
+        $divisores = 0;
+        echo "<p>Lendo o número $valor</p>";
+        for ($num = $valor; $num >= 1; $num--) {
+            $valor % $num == 0 ? $divisores++ : '';
 
+            if ($divisores > 2) {
+                echo "<p>O número $valor <b>não</b> é primo;";
+                $existe_primo = false;
+                
+                break;
+            }
+        }
+        
+        $divisores == 2 ? $primo[] = $valor : '';
+        $divisores == 2 ? $existe_primo = true : $existe_primo = false;
+    }
+    echo "<hr>";
+    if ($existe_primo == true) {
+        echo "<p>Os números primos são:</p>";
 
+        foreach ($primo as $chave => $valor) {
+            echo $valor != null ? "$valor - " : "Não há números primos";
+        }
+    } else {
+        echo "Não há números primos";
+    }
+    ?>
+</body>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // for ($i = 0; $i < $numero; $i++) {
-// // $vetor_primos[$i] = null;
-// // }
-
-// foreach ($vetor as $chave => $valor) {
-//     $num = rand(1, 10);
-
-//     $vetor[$chave] = $num;
-//     echo "var_dump($vetor[$chave])<br>";
-//     $i = 1;
-//     while ($i < $vetor[$chave]) {
-//         $i++;
-//     }
-//     $primo = $vetor[$chave] % $i;
-//     // echo var_dump($primo);
-//     $vetor_primos[] = '';
-//     $vetor_primo[$chave] = $primo;
-//     // echo var_dump($vetor_primo[$chave]);
-//     // echo "<pre>";
-//     // echo var_dump($vetor_primo[$chave]);
-//     // echo "</pre>";
-//     // echo $vetor[$i];
-// }
+</html>
---
+++ Fevereiro/Aula 05/3.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<!-- Média, Moda e Mediana:
+a. Crie um vetor de 15 posições e preencha com valores aleatórios entre 0 e 9
+b. Calcule a Média, a Moda e a Mediana desse vetor -->
+
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Média, moda e mediana</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Média, moda e mediana</h1>
+    <?php
+
+    $numero = 15;
+
+    for ($i = 0; $i < $numero; $i++) {
+        $vetor[$i] = rand(0, 9);
+    }
+
+    #Mediana
+    sort($vetor);
+    echo "O array é: ";
+    echo "<pre>";
+    print_r($vetor);
+    echo "</pre>";
+    //Conta o tamanho do array
+    $tamanho_array = count($vetor);
+    //Descobre a posição que é metade do array
+    $meio_array = (int)($tamanho_array / 2);
+    //Calculo da mediana
+    $mediana = $vetor[$meio_array];
+
+    #Média
+    $soma = 0;
+    foreach ($vetor as $valor) {
+        $soma += $valor;
+    }
+    $media = $soma / 15;
+
+    $moda = 0;
+    #Moda
+    $ocorrencia = [];
+    foreach ($vetor as $valor) {
+        // $ocorrencia = [];
+        if (!isset($ocorrencia[$valor])) {
+            $ocorrencia[$valor] = 0;
+        }
+        $ocorrencia[$valor]++;
+        // echo "<p>Ocorrencia do $valor: $ocorrencia[$valor]</p>";
+        $moda = array_keys($ocorrencia, max($ocorrencia));
+    }
+    $moda = implode(' - ', $moda);
+
+    echo "<hr>
+        <h3>Média</h3> 
+        <p>
+            A média é: <b>$media</b>
+        </p>
+        <hr>";
+
+    echo "<hr>
+        <h3>Moda</h3> 
+        <p>
+            A(s) moda(s) é/são: <b>$moda</b>
+        </p>
+        <hr>";
+
+    echo "<hr>
+        <h3>Mediana</h3> 
+        <p>
+            A média é: <b>$mediana</b>
+        </p>
+        <hr>";
+
+    ?>
+</body>
+
+</html>
