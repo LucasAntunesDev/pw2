fdc91362819e5a1f1c46c6b688d3cb5c1b61917a
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-03-20 11:15:49 -0300

    update exercício 1

--- Março/Lista 7/exe1/atividade1.php
+++
@@ -1,6 +0,0 @@
-<?php
-//atividade1.php
-$numero = $_POST['num'];
-if($numero >= 10) echo  'O número é maior que 10' ;
-else if($numero == 10 ) echo 'O número é menor que 10';
-else echo 'O número é igual a 10';
