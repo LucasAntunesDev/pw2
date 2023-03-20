19c01810a9ac1e162ea6a431ec3f9fbc8c4c5a31
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-03-20 16:26:58 -0300

    ex 2

---
+++ Março/Aula 10/exe2/atividade2.php
@@ -0,0 +1,13 @@
+<?php
+//atividade2.php
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+$nota4 = $_POST['nota4'];
+
+$soma = $nota1 + $nota2 + $nota3 + $nota4;
+$media = $soma / 4;
+
+echo $media >= 7.0 ? "Parabéns! Você foi aprovado =D! Sua média é <b>$media<b>" : "Você foi reprovado :( Sua média é <b>$media<b>";
+
+echo "<br><a href='index.html'>Voltar para a página inicial</a>";
