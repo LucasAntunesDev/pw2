8e722a5cb68839001d27deb8fe5ab2383915019e
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-03-20 17:21:00 -0300

    little updates

--- Março/Aula 10/exe5/atividade5.php
+++ Março/Aula 10/exe5/atividade5.php
@@ -1,5 +1,5 @@
 <?php
-//atividade4.php
+//atividade5.php
 $frase = strtoupper($_POST['frase']);
 $caracter = strtoupper($_POST['caracter']);
 $ocorrencia = substr_count($frase, $caracter);
@@ -10,7 +10,7 @@ $caracter = $_POST['caracter'];
 echo "<h1>Ocorrência de caracter</h1>";
 echo "A letra \"<b>$caracter</b>\" aparece <b>$ocorrencia</b> vezes na frase <b>\"$frase\"</b>";
 
-echo "<br></br>
+echo "<br>
     <a href='index.html'>
         <button>
             Voltar para a página inicial
