ba17bb5f85e0b71b8d095bffe5e48a3b74b29855
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-03-23 11:15:20 -0300

    little updates

--- Março/Lista 7/exe2/atividade2.php
+++ Março/Lista 7/exe2/atividade2.php
@@ -6,7 +6,7 @@ $nota3 = $_POST['nota3'];
 $nota4 = $_POST['nota4'];
 
 $soma = $nota1 + $nota2 + $nota3 + $nota4;
-$media = $soma / 4;
+$media = round($soma / 4, 2);
 
 echo $media >= 7.0 ? "Parabéns! Você foi aprovado =D! Sua média é <b>$media<b>" : "Você foi reprovado :( Sua média é <b>$media<b>";
 
--- Março/Lista 7/exe5/atividade5.php
+++ Março/Lista 7/exe5/atividade5.php
@@ -2,23 +2,21 @@
 //atividade5.php
 $array = [36, 6, 7, 9, 87, 21, 32, 31, 33, 44, 55, 66, 77, 98, 12, 123, 229, 17, 75, 22];
 
-sort($array);
-
-$maior = '';
+$maior = 0;
 $menor = $array[1];
 $soma = 0;
 $media = 0;
 
-foreach($array as $valor){
+foreach ($array as $valor) {
     $valor < $menor ? $menor = $valor : '';
     $valor > $maior ? $maior = $valor : '';
     $soma += $valor;
 }
 
-$media = round($soma/count($array), 2);
+$media = round($soma / count($array), 2);
 
 echo '<pre>';
-echo "<p>O maior valor é $maior</p>";
-echo "<p>O menor valor é $menor</p>";
-echo "<p>A média é $media</p>";
+echo "<p>O maior valor é <b>$maior</b></p>";
+echo "<p>O menor valor é <b>$menor</b></p>";
+echo "<p>A média é <b>$media</b></p>";
 echo '</pre>';
