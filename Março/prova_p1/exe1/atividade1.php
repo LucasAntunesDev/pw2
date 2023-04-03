eed42f7993ff95792332ee2efe575f4d402bc355
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-04-03 17:16:24 -0300

    little updates

--- Março/prova_p1/exe1/atividade1.php
+++ Março/prova_p1/exe1/atividade1.php
@@ -1,5 +1,5 @@
 <?php
 $num = $_POST['num'];
-if($num > 0) echo "O número $num é positivo!";
-else if($num < 0) echo "O número $num é negativo!";
+if ($num > 0) echo "O número $num é positivo!";
+else if ($num < 0) echo "O número $num é negativo!";
 else echo "O número $num é igual a zero!";
--- Março/prova_p1/exe2/atividade2.php
+++ Março/prova_p1/exe2/atividade2.php
@@ -1,7 +1,7 @@
 <?php
 $num = $_POST['num'];
-if($num % 2 == 0) echo "O número $num é divisível por 2<br>";
-if($num % 5 == 0) echo "O número $num é divisível por 5<br>";
-if($num % 10 == 0) echo "O número $num é divisível por 10<br>";
-if($num % 2 != 0 && $num % 5!= 0 && $num % 10 != 0) echo "O número $num não é divisível
-nem por 2, 5 ou 10!";
+if ($num % 2 == 0) echo "<p>O número $num é divisível por 2</p>";
+if ($num % 5 == 0) echo "<p>O número $num é divisível por 5</p>";
+if ($num % 10 == 0) echo "<p>O número $num é divisível por 10</p>";
+if ($num % 2 != 0 && $num % 5 != 0 && $num % 10 != 0)
+    echo "<p>O número $num não é divisível nem por 2, 5 ou 10!</p>";
