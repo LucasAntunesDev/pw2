48eb7a907b73ae524b63c5daf799f876bef040bd
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-04-10 12:16:58 -0300

    update

--- 04 - Abril/Lista 09 - JS/exe2/script.js
+++ 04 - Abril/Lista 09 - JS/exe2/script.js
@@ -1,38 +1,40 @@
-const validar = () =>{
-    // b.preventDefault();
+const validar = () => {
+ const nome = document.getElementById('nome');
+ const idade = document.getElementById('idade');
+ const sexo = document.getElementById('sexos');
+ const cidade = document.getElementById('cidade');
 
-    // const inputNome = document.getElementById('nome');
-    // const inputIdade = document.getElementById('idade');
-    const nome = document.getElementById("nome");
-    const idade = document.getElementById("idade");
-    
-    nome.style.background = 'white';
-    idade.style.background = 'white';
-    
-    if(nome.value.length === 0){
-        nome.style.background = 'red';
-        alert('O campo está vazio!');
-        nome.focus();
-        return false;
-    }
-    if(idade.value.length === 0){
-        idade.style.background = 'red';
-        alert('O campo está vazio!');
-        idade.focus();
-        return false;
-    }
-    else if(nome.value.length < 4){
-        nome.style.background = 'red';
-        alert('O nome não está completo!');
-        nome.focus();
-        return false;
-    }
-    else if(idade.value > 18 || idade.value < 0){
-        idade.style.background = 'red';
-        alert('Você é maior de 18 anos!');
-        idade.focus();
-        return false;
-    }
+ nome.style.background = 'white';
+ idade.style.background = 'white';
 
-    return true;
-}
+ if (nome.value.length === 0) {
+  nome.style.background = 'red';
+  alert('O campo nome está vazio!');
+  nome.focus();
+  return false;
+ }
+ else if (idade.value.length === 0) {
+  idade.style.background = 'red';
+  alert('O campo idade está vazio!');
+  idade.focus();
+  return false;
+ }
+ else if (nome.value.length < 4) {
+  nome.style.background = 'red';
+  alert('O nome não está completo!');
+  nome.focus();
+  return false;
+ }
+ else if (idade.value > 18 || idade.value < 0) {
+  idade.style.background = 'red';
+  alert('Você é maior de 18 anos!');
+  idade.focus();
+  return false;
+ }
+ else if(cidade.value == '-1'){
+    alert('selecione uma cidade');
+    return false;
+ }
+
+ return true;
+};
---
+++ 04 - Abril/Lista 09 - JS/exe5/script5.js
@@ -0,0 +1,34 @@
+const validar = () =>{
+    const nome = document.getElementById("nome");
+    const idade = document.getElementById("idade");
+    
+    nome.style.background = 'white';
+    idade.style.background = 'white';
+    
+    if(nome.value.length === 0){
+        nome.style.background = 'red';
+        alert('O campo está vazio!');
+        nome.focus();
+        return false;
+    }
+    if(idade.value.length === 0){
+        idade.style.background = 'red';
+        alert('O campo está vazio!');
+        idade.focus();
+        return false;
+    }
+    else if(nome.value.length < 4){
+        nome.style.background = 'red';
+        alert('O nome não está completo!');
+        nome.focus();
+        return false;
+    }
+    else if(idade.value > 18 || idade.value < 0){
+        idade.style.background = 'red';
+        alert('Você é maior de 18 anos!');
+        idade.focus();
+        return false;
+    }
+
+    return true;
+}
