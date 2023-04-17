c24c8a43357612a80c43fd078db70e110d0d2b50
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-04-17 16:30:48 -0300

    lista 10 - exercício 04

--- 04 - Abril/Lista 09 - JS/exe4/script4.js
+++ 04 - Abril/Lista 09 - JS/exe4/script4.js
@@ -1,17 +1,19 @@
-const validar = (estado) => {
-//   const estado = document.getElementById("estados").value;
+const validar = () =>{
+  const rs = document.getElementById('rs').value;
+  const sc = document.getElementById('sc').value;
+  const pr = document.getElementById('pr').value;
+  const estado = document.getElementById('estados').value;
 
-  switch (estado) {
-    case rs:
-      alert("Bem vindos ao RS");
-      break;
-    case sc:
-      alert("Bem vindos a SC");
-      break;
-    case pr:
-      alert("Bem vindos ao PR");
-      break;
+  switch (estado){
+      case rs:
+          alert('Bem vindos ao RS');
+          break;
+      case sc:
+          alert('Bem vindos a SC');
+          break;
+      case pr:
+          alert('Bem vindos ao PR');
   }
 
-  return false;
-};
+  return false
+}
--- 04 - Abril/Lista 10 - JS II/exe4/exe4.js
+++ 04 - Abril/Lista 10 - JS II/exe4/exe4.js
@@ -1,22 +1,41 @@
-alert("Olá pessoal. Sejam bem-vindos à aula de Programação Web II");
+const validar = () => {
+  const machado = document.getElementById('machado');
+  const alencar = document.getElementById('jose-de-alencar');
+  const pessoa = document.getElementById('fernando-pessoa');
+  const camoes = document.getElementById('camoes');
+  const eco = document.getElementById('eco');
+  const select = document.getElementById('escritores').value;
+  const nome = document.getElementById('nome');
+  const nasc = document.getElementById('nasc');
+  const obra = document.getElementById('obra');
 
-const x = parseInt(prompt("Digite o primeiro número"));
+  switch (select) {
+    case machado.value:
+      nome.innerHTML = `<h1>${machado.innerHTML}</h1>`;
+      nasc.innerHTML = '<h3>Data de nascimento: 1839</h3>'
+      obra.innerHTML = '<h3>Principal  obra: Dom Casmurro</h3>'
+      break;
+    case alencar.value:
+      nome.innerHTML = `<h1>${alencar.innerHTML}</h1>`;
+      nasc.innerHTML = '<h3>Data de nascimento: 1829</h3>'
+      obra.innerHTML = '<h3>Principal  obra: Iracema</h3>'
+      break;
+    case pessoa.value:
+      nome.innerHTML = `<h1>${pessoa.innerHTML}</h1>`;
+      nasc.innerHTML = '<h3>Data de nascimento: 1888</h3>'
+      obra.innerHTML = '<h3>Principal  obra: Mensagem</h3>'
+      break;
+    case camoes.value:
+      nome.innerHTML = `<h1>${camoes.innerHTML}</h1>`
+      nasc.innerHTML = '<h3>Data de nascimento: 1580</h3>'
+      obra.innerHTML = '<h3>Principal  obra: Os Lusíadas</h3>'
+      break;
+    case eco.value:
+      nome.innerHTML = `<h1>${eco.innerHTML}</h1>`;
+      nasc.innerHTML = '<h3>Data de nascimento: 1932</h3>'
+      obra.innerHTML = '<h3>Principal  obra: O Nome da Rosa</h3>'
+      break;
+  }
 
-const y = parseInt(prompt("Digite o segundo número"));
-
-const opercao = prompt("Digite a operação que deseja realizar (+, -, * ou /)");
-
-switch (opercao) {
-  case "+":
-    alert(x + y);
-    break;
-  case "-":
-    alert(x - y);
-    break;
-  case "*":
-    alert(x * y);
-    break;
-  case "/":
-    alert(x / y);
-    break;
+  return false;
 }
