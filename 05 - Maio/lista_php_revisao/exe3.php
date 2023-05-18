afd090af7aa2d4b62bb74f93ebe3c150d9c85be2
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-05-18 11:04:09 -0300

    exe 3 18.05.23

---
+++ 05 - Maio/lista_php_revisao/exe3.php
@@ -0,0 +1,10 @@
+<?php
+function celcius ($f) {
+    return round(($f-32)/1.8, 2);
+}
+
+echo "90º F em Celsius são <b>". celcius(90)."</b>°C <br>";
+
+echo "77º F em Celsius são <b>". celcius(77)."</b>°C <br>";
+
+echo "12º F em Celsius são <b>". celcius(12)."</b>°C <br>";
