5add64aa1360ae23fd5eba67935354ef5bffc132
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-05-30 15:06:22 -0300

    add: exercício 1 (30.05.23)

---
+++ 05 - Maio/30.05 - Revisao_JS/exe1/script.js
@@ -0,0 +1,6 @@
+let distancia = parseInt(prompt('Entre com a distância:'))
+let tempo = parseInt(prompt('Entre com o tempo:'))
+
+let velMedia = distancia/tempo
+
+alert(`A distância foi de ${velMedia}m/s`)
