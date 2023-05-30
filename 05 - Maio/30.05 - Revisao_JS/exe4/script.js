97ab84e75460a13d8a45d748357f23f125a747b1
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-05-30 15:34:42 -0300

    add: exercício  4 (30.05.23)

--- 05 - Maio/30.05 - Revisao_JS/exe4/script.js
+++ 05 - Maio/30.05 - Revisao_JS/exe4/script.js
@@ -1,6 +1,10 @@
-let nota1 = parseInt(prompt('Entre com a nota 1:'))
-let nota2 = parseInt(prompt('Entre com a nota 2:'))
-let nota3 = parseInt(prompt('Entre com a nota 3:'))
+var parar = false
 
-let media = Math.round(((nota1+nota2+nota3) / 3),2)
-alert(`A média foi de ${media}`)
+while(parar == false){
+    let num = parseInt(prompt('Entre com o número:'))
+    
+    num % 2 == 0 ? alert(`O número ${num} é par`) : alert(`O número ${num} é ímpar`)
+
+    let verificaParar = confirm('Você deseja digitar mais algum número?')
+    verificaParar ? '' : parar = true
+}
