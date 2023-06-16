d98d67dfd7032efba56d50215d3f2822cf58ba87
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-06-16 16:38:11 -0300

    update: exercício 02

--- 06 - Junho/06.06 - jQuery/exe2/script.js
+++ 06 - Junho/06.06 - jQuery/exe2/script.js
@@ -1,3 +1,20 @@
-const cpfInput =  $('$cpf')
-const cpfInput =  $('$cpf')
-const cpfInput =  $('$cpf')
+const validar = () =>{
+    const nomeInput =  $('#nome')
+    const cpfInput =  $('#cpf')
+    const senhaInput =  $('#senha')
+
+    if(nomeInput.val().length < 3) {
+        alert ('Você precisa digitar no mínimo 3 caracteres')
+        return false;
+    }
+    if(cpfInput.val().length < 11 && typeof cpfInput != 'string') {
+        if(cpfInput.val().length < 11) alert ('Você precisa digitar no mínimo 11 caracteres e apenas números')
+        cpfInput.focus()
+        cpfInput.addClass('erro')
+        return false;
+    }
+    if(senhaInput.val().length < 6) {
+        alert ('Você precisa digitar no mínimo 6 caracteres')
+        return false;
+    }
+}
