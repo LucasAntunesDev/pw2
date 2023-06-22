e7c900cd63669ce846598549a5a8ba0aebf82a0a
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-06-22 11:11:57 -0300

    fix: mensagens na pagina.php (exe1)

--- 06 - Junho/trabalho_01/exe1/pagina.php
+++ 06 - Junho/trabalho_01/exe1/pagina.php
@@ -6,10 +6,10 @@ $senha = $_GET['senha'];
 
 echo "<link rel='stylesheet' href='estilo.css' />";
 echo "<ul>
-        <li>$nome</li>  
-        <li>$senha</li>  
-        <li>$login</li>  
-        <li>$senha</li>  
+        <li>Nome: $nome</li>  
+        <li>CPF: $cpf</li>  
+        <li>Login: $login</li>  
+        <li>Senha: $senha</li>  
       </ul>
 ";
 
