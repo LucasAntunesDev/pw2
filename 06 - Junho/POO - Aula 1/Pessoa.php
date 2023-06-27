82f2bb0875785af18ee8c3811a797a53d0aa6aae
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-06-27 15:09:55 -0300

    add: construct - Aula POO 1

--- 06 - Junho/POO - Aula 1/Pessoa.php
+++ 06 - Junho/POO - Aula 1/Pessoa.php
@@ -5,6 +5,12 @@ class Pessoa
     public $nome;
     public $idade;
 
+    public function __construct($nome, $idade = 0)
+    {
+        $this->nome = $nome;
+        $this->idade = $idade;
+    }
+
     public function fazerAniversario()
     {
         $this->idade++;
--- 06 - Junho/POO - Aula 1/index.php
+++ 06 - Junho/POO - Aula 1/index.php
@@ -2,4 +2,23 @@
 
 require_once('Pessoa.php');
 
-$nome = new Pessoa();
+// $pessoa1 = new Pessoa();
+// $pessoa1->nome = 'Lucas';
+// $pessoa1->idade = 17;
+
+$pessoa1 = new Pessoa('Lucas', 17);
+
+
+echo "Nome: $pessoa1->nome";
+echo '<br>';
+echo "Idade de $pessoa1->nome: $pessoa1->idade";
+
+// $pessoa1->fazerAniversario();
+// echo '<br>';
+// echo "$pessoa1->nome fez aniversário: $pessoa1->idade";
+
+// $pessoa2 = new Pessoa();
+// $pessoa2->nome = 'Enner Valencia';
+// $pessoa2->idade = 33;
+// echo '<br>';
+// echo "Idade de $pessoa2->nome: $pessoa2->idade";
