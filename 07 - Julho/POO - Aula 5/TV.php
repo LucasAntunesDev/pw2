0fc56cb697aeaa790078e33f9f296416c3f3f64b
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-07-11 15:43:30 -0300

    add: Aula POO 5

---
+++ 07 - Julho/POO - Aula 5/TV.php
@@ -0,0 +1,65 @@
+<?php
+
+class TV{
+    protected string $marca;
+    protected int $polegadas;
+    protected bool $ligada;
+    protected int $canal;
+    protected int $volume;
+
+    public function __construct($marca, $polegadas){
+        $this->ligada = false;
+        $this->volume = 10;
+        $this->canal = 12;
+        $this->marca = $marca;
+        $this->polegadas = $polegadas;
+    }
+
+    public function getMarca(){
+        return $this->marca;
+    }
+
+    public function setMarca($marca){
+        $this->marca = $marca;
+    }
+
+    public function getPolegadas(){
+        return $this->polegadas;
+    }
+
+    public function setPolegadas($polegadas){
+        $this->polegadas = $polegadas;
+    }
+    
+    public function isLigada(){
+        return $this->ligada;
+    }
+
+    public function getCanal(){
+        return $this->canal;
+    }
+
+    public function getVolume(){
+        return $this->volume;
+    }
+
+    public function ligar(){
+        $this->ligada = true;
+    }
+
+    public function desligar(){
+        $this->ligada = false;
+    }
+
+    public function aumentarVolume(){
+        $this->volume =+ $this->volume;
+    }
+
+    public function diminiurVolume(){
+        $this->volume =- $this->volume;
+    }
+
+    public function  trocarCanal($canal){
+        $this->canal = $canal;
+    }
+}
