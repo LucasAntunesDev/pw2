31db05ccd6b92d2fc2f00238f95e5778ac943776
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-08-10 09:57:38 -0300

    update: pdf do mpdf

--- semestre_2/08 - Agosto/Aula Composer/mpdf/index.php
+++ semestre_2/08 - Agosto/Aula Composer/mpdf/index.php
@@ -7,5 +7,6 @@ require_once("vendor/autoload.php");
 $mpdf = new \Mpdf\Mpdf();
 
 $mpdf->WriteHTML("<h1>Sport Club Internacional</h1>");
+$mpdf->WriteHTML("<img src='https://live.staticflickr.com/65535/53103833904_6fa81122f2_b.jpg'/>");
 
 $mpdf->Output();
