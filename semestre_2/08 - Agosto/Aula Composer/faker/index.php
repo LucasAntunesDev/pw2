2cc739d17e316e87673f5886e17aa2c7c1d21795
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-08-10 09:15:58 -0300

    add: faker - aula composer

---
+++ semestre_2/08 - Agosto/Aula Composer/faker/index.php
@@ -0,0 +1,14 @@
+<?php
+//index.php
+
+require_once('vendor/autoload.php');
+
+$faker = Faker\Factory::create('pt_BR');
+
+echo $faker->name();
+echo '<br>';
+echo $faker->email();
+echo '<br>';
+echo $faker->city();
+echo '<br>';
+echo $faker->state();
