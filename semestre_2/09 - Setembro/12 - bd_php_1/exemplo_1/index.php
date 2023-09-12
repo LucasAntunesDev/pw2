cc5190f511c6e3247d104308cb7f420edd7f6b75
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-09-12 14:34:29 -0300

    update: exemplo 1 - aula bd php 1

--- semestre_2/09 - Setembro/12 - bd_php_1/exemplo_1/index.php
+++ semestre_2/09 - Setembro/12 - bd_php_1/exemplo_1/index.php
@@ -7,4 +7,31 @@ $query = 'SELECT * FROM CARROS';
 $statement = $pdo->prepare($query);
 $statement->execute();
 $carros = $statement->fetchAll();
-print_r($carros);
+// echo '<pre>';
+// print_r($carros);
+// echo '</pre>';
+
+echo '<table border="1">';
+    echo '<tr>
+            <th>ID</th>    
+            <th>Marca</th>    
+            <th>Modelo</th>
+            <th>Ano</th>  
+            <th>Placa</th> 
+            <th>Chassi</th>
+            <th>Cor</th>
+        </tr>';
+
+    foreach($carros as $carro){
+        echo '<tr>';
+            echo "<td>" . $carro['id']. "</td>";
+            echo "<td>" . $carro['marca']. "</td>";
+            echo "<td>" . $carro['modelo']. "</td>";
+            echo "<td>" . $carro['ano']. "</td>";
+            echo "<td>" . $carro['placa']. "</td>";
+            echo "<td>" . $carro['chassi']. "</td>";
+            echo "<td>" . $carro['cor']. "</td>";
+        echo '</tr>';
+    }
+
+echo '</table>';
