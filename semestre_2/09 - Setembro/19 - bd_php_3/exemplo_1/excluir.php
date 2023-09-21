32c06d717bbec4b524da4de7e3fcf8ce5489b15a
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-09-21 08:44:02 -0300

    add: correção excluir.php

--- semestre_2/09 - Setembro/19 - bd_php_3/exemplo_1/excluir.php
+++ semestre_2/09 - Setembro/19 - bd_php_3/exemplo_1/excluir.php
@@ -4,6 +4,11 @@
 require_once('Database.php');
 $db = new Database();
 
+if(!isset($_GET['id'])) {
+    header('location:index.php');
+    die;
+}
+
 $query = 'DELETE FROM carros WHERE id = :id';
 
 $binds = [':id' => $_GET['id']];
