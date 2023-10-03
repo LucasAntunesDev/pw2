b1b7554d0790a1c51d24be99f73009691be5b86c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-03 14:15:55 -0300

    add abstract Controller

---
+++ semestre_2/10 - Outubro/mvc/controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+//Controller.php
+
+namespace Controller;
+
+abstract class Controller{
+    public function __construct(){
+    #Será feita aqui, posteriormente, a autenticação (autorização)   
+    }
+
+    public function redirect($url){
+        header("location: $url");
+    }
+
+    public function loadView($view, $data = []){
+        #Esse método cria automaticamente variáveis baseados nos indexes do array
+        extract($data);
+        include("views/$view.php");
+    }
+
+}
