6480aad07d1b48617666b51fc45c4bfafa940137
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-03 14:53:08 -0300

    add: listaDisciplinas

---
+++ semestre_2/10 - Outubro/mvc/views/listaDisciplinas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disciplinas</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+                foreach($disciplinas as $disciplina){
+                    echo '<tr>';
+                        echo '<td>' .$disciplina->getId() .'</td>';
+                        echo '<td>' .$disciplina->getNome() .'</td>';
+                        echo '<td></td>';
+                    echo '</tr>';
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
