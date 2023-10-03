10570e88ee089a90e8e5a039390e26d6c04e61dc
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-03 14:37:29 -0300

    add: DisciplinaModel

---
+++ semestre_2/10 - Outubro/mvc/models/DisciplinaModel.php
@@ -0,0 +1,39 @@
+<?php
+//DisciplinaModel.php
+
+namespace Model;
+use Model\VO\DisciplinaVO;
+use Util\Database;
+
+final class DisciplinaModel extends Model{
+    public function selectAll($vo){
+        $db = new Database();
+        $data = $db->select('SELECT * FROM disciplinas');
+
+        $array = [];
+
+        foreach($data as $row){
+            $vo = new DisciplinaVO($row['id'],$row['nome']);
+            array_push($array, $vo);
+        }
+
+        return $array;
+
+    }
+
+    public function selectOne($vo){
+
+    }
+
+    public function insert($vo){
+
+    }
+
+    public function delete($vo){
+
+    }
+
+    public function update($vo){
+
+    }
+}
