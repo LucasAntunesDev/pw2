679b5762cbdf9f5b5a8cb1d3174677d74182a544
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-05 09:14:48 -0300

    add: selectOne em DisicplinasModel

--- semestre_2/10 - Outubro/mvc/models/DisciplinaModel.php
+++ semestre_2/10 - Outubro/mvc/models/DisciplinaModel.php
@@ -2,38 +2,43 @@
 //DisciplinaModel.php
 
 namespace Model;
+
 use Model\VO\DisciplinaVO;
 use Util\Database;
 
-final class DisciplinaModel extends Model{
-    public function selectAll($vo = null){
+final class DisciplinaModel extends Model {
+    public function selectAll($vo = null) {
         $db = new Database();
         $data = $db->select('SELECT * FROM disciplinas');
 
         $array = [];
 
-        foreach($data as $row){
-            $vo = new DisciplinaVO($row['id'],$row['nome']);
-            array_push($array, $vo) ;
+        foreach ($data as $row) {
+            $vo = new DisciplinaVO($row['id'], $row['nome']);
+            array_push($array, $vo);
         }
 
         return $array;
-
     }
 
-    public function selectOne($vo){
+    //conecta no bd, executa a query e retorna os dados
+    public function selectOne($vo = null) {
+        $db = new Database();
+        $query = 'SELECT * FROM disciplinas WHERE id = :id';
+        $binds = [':id' => $vo->getId()];
+        $data = $db->select($query, $binds);
 
+        if(count($data) === 0) return null;
+
+        return new DisciplinaVO($data[0]['id'], $data[0]['nome']);
     }
 
-    public function insert($vo){
-
+    public function insert($vo = null) {
     }
 
-    public function delete($vo){
-
+    public function delete($vo = null) {
     }
 
-    public function update($vo){
-
+    public function update($vo = null) {
     }
 }
