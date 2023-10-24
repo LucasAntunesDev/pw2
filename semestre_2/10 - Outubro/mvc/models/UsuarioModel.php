3c98667b3c53d1cbf07c4b837f5091e8ed302a5c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-24 14:37:40 -0300

    add: endpoint do login

--- semestre_2/10 - Outubro/mvc/models/UsuarioModel.php
+++ semestre_2/10 - Outubro/mvc/models/UsuarioModel.php
@@ -82,7 +82,7 @@ final class UsuarioModel extends Model {
 
     public function doLogin($vo) {
         $db = new Database();
-        $query = 'SELECT * FROM usuarios WHERE login =:login AND senha = :senha';
+        $query = 'SELECT * FROM usuarios WHERE login = :login AND senha = :senha';
         $binds = [
             ':login' => $vo->getLogin(),
             ':login' => sha1($vo->getSenha())
