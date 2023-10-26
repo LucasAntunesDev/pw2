df8a19061c6813965d99bf11d9f97fa96916a370
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-26 12:25:53 -0300

    update: LivroController

--- semestre_2/10 - Outubro/trabalho_biblioteca/controllers/LivroController.php
+++ semestre_2/10 - Outubro/trabalho_biblioteca/controllers/LivroController.php
@@ -2,55 +2,53 @@
 namespace Controller;
 
 use Embed\Http\Redirects;
-use Model\DisciplinaModel;
-use Model\VO\DisciplinaVO;
+use Model\LivroModel;
+use Model\VO\LivroVO;
 
 
-final class DisciplinaController extends Controller {
+final class LivroController extends Controller {
     public function list() {
-        $model  = new DisciplinaModel();
+        $model  = new LivroModel();
         $data = $model->selectAll();
 
-        $this->loadView('listaDisciplinas', [
-            'disciplinas' => $data
+        $this->loadView('listaLivros', [
+            'livros' => $data
         ]);
     }
 
-    //monta o formulário
     public function get() {
-        //versões maiores que 7.2
         $id = $_GET['id'] ?? null;
 
-        //versões menores que 7.2
-        // $id = (isset($_GET['id'])) ? $_GET['id'] : null;
-
         if (empty($id)) {
-            $vo = new DisciplinaVO();
+            $vo = new LivroVO();
         } else {
-            $model = new DisciplinaModel();
-            $vo = $model->selectOne(new DisciplinaVO($id));
+            $model = new LivroModel();
+            $vo = $model->selectOne(new LivroVO($id));
         }
 
-        $this->loadView('formDisciplina', ['disciplina' => $vo]);
+        $this->loadView('formLivro', ['livro' => $vo]);
     }
 
     public function save() {
         $id = $_POST['id'];
-        $vo = new DisciplinaVO($_POST['id'], $_POST['nome']);
-        $model = new DisciplinaModel();
+        $vo = new LivroVO(
+                            $_POST['id'], $_POST['titulo'], $_POST['autores'], $_POST['editora'], 
+                            $_POST['qtd_exmemplares'], $_POST['data_retirada'], $_POST['data_devolucao']
+                        );
+        $model = new LivroModel();
 
         $return = empty($id) ? $model->insert($vo) : $model->update($vo);
 
-        $this->redirect('disciplinas.php');
+        $this->redirect('livros.php');
     }
 
     public function remove() {
         if(empty($_GET['id'])) die('Necessário passar o ID');
 
-        $model = new DisciplinaModel();
+        $model = new LivroModel();
 
-        $return = $model->delete(new DisciplinaVO($_GET['id']));
+        $return = $model->delete(new LivroVO($_GET['id']));
 
-        $this->redirect('disciplinas.php');
+        $this->redirect('livros.php');
     }
 }
