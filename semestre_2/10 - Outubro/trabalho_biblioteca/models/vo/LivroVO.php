75f46ac185d886bbdb1a578039e99a8d51dcd351
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-26 12:34:02 -0300

    update: LivroController, LivroVo

--- semestre_2/10 - Outubro/trabalho_biblioteca/models/vo/LivroVO.php
+++ semestre_2/10 - Outubro/trabalho_biblioteca/models/vo/LivroVO.php
@@ -1,21 +1,75 @@
 <?php
-//DisciplinaVO.php
-
 namespace Model\VO;
 
-final class DisciplinaVO extends VO {
-    private $nome;
+final class LivroVO extends VO {
+    private $titulo;
+    private $autores;
+    private $editora;
+    private $qtd_exemplares;
+    private $data_retirada;
+    private $data_devolucao;
+    private $isbn;
 
-    public function __construct($id = 0, $nome = '') {
+    public function __construct(
+                                $id = 0, $titulo = '', $autores = '', $editora = '', 
+                                $qtd_exemplares = '', $data_retirada = '', $data_devolucao = '', $isbn = ''
+                                ) {
         parent::__construct($id);
-        $this->nome = $nome;
+        $this->titulo = $titulo;
     }
 
-    public function getNome() {
-        return $this->nome;
+    public function getTitulo() {
+        return $this->titulo;
     }
 
-    public function setNome($nome) {
-        $this->nome = $nome;
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    public function getAutores() {
+        return $this->autores;
+    }
+
+    public function setAutores($autores) {
+        $this->autores = $autores;
+    }
+    public function getEditora() {
+        return $this->editora;
+    }
+
+    public function setEditora($editora) {
+        $this->editora = $editora;
+    }
+
+    public function getQuantidadeExemplares() {
+        return $this->qtd_exemplares;
+    }
+
+    public function setQuantidadeExemplares($qtd_exemplares) {
+        $this->qtd_exemplares = $qtd_exemplares;
+    }
+
+    public function getDataRetirada() {
+        return $this->data_retirada;
+    }
+
+    public function setDataRetirada($data_retirada) {
+        $this->data_retirada = $data_retirada;
+    }
+
+    public function getDataDevolucao() {
+        return $this->data_devolucao;
+    }
+
+    public function setDataDevolucao($data_devolucao) {
+        $this->data_devolucao = $data_devolucao;
+    }
+
+    public function getISBN() {
+        return $this->isbn;
+    }
+
+    public function setISBN($isbn) {
+        $this->isbn = $isbn;
     }
 }
