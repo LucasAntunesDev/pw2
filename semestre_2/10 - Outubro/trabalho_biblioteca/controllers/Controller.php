123e632d4874313feaede62fb95e26c331959802
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-26 10:02:36 -0300

    update: Controller

--- semestre_2/10 - Outubro/trabalho_biblioteca/controllers/Controller.php
+++ semestre_2/10 - Outubro/trabalho_biblioteca/controllers/Controller.php
@@ -2,15 +2,15 @@
 
 namespace Controller;
 
-use Model\UsuarioModel;
-use Model\VO\UsuarioVO;
+use Model\UsuarioAdmnistrativoModel;
+use Model\VO\UsuarioAdmnistrativoVO;
 
 abstract class Controller {
     public function __construct($obrigaLogin = true) {
         session_start();
 
         if ($obrigaLogin) {
-            $model = new UsuarioModel();
+            $model = new UsuarioAdmnistrativoModel();
 
             if (!$model->checkLogin()) $this->redirect('login.php');
             
