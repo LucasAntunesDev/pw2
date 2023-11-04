988184f6319a6df8cdad87b4e36021a1834ca022
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-04 07:06:53 -0300

    update: menu

--- semestre_2/10 - Outubro/trabalho_biblioteca/views/includes/menu.php
+++ semestre_2/10 - Outubro/trabalho_biblioteca/views/includes/menu.php
@@ -1,26 +1,35 @@
-<div class="flex flex-row items-center pl-4 shadow-md gap-x-4 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<div class="flex flex-row justify-between items-center pl-4 shadow-md gap-x-4 
 h-16 bg-sky-600 text-zinc-50 border-b-[3px] border-sky-400">
 
-<p>Olá!</p>
-<ul class="flex flex-row gap-x-4">
+<p class="pl-10">👋 Olá!</p>
+<ul class="flex flex-row gap-x-4 pr-10">
     
-    <li class="w-min flex hover:underline transition duration-300 ease-in-out">
-        <a href="livros.php">Livros</a>
+    <li class="w-fit flex hover:underline transition duration-300 ease-in-out items-center gap-x-1">
+        <a href="livros.php">
+            <i class="fa-solid fa-book"></i>
+            Livros
+        </a>
     </li>
-
-    <li class="w-min flex hover:underline transition duration-300 ease-in-out">
+    
+    <li class="w-fit flex hover:underline transition duration-300 ease-in-out items-center gap-x-1">
+        <i class="fa-solid fa-graduation-cap"></i>
         <a href="alunos.php">Alunos</a>
     </li>
 
-    <li class="w-fit flex hover:underline transition duration-300 ease-in-out">
+    <li class="w-fit flex hover:underline transition duration-300 ease-in-out items-center gap-x-1">
+        <i class="fa-solid fa-circle-user"></i>
         <a href="usuariosAdmnistrativos.php">Usuarios Admnistrativos</a>
     </li>
 
-    <li class="w-min flex hover:underline transition duration-300 ease-in-out">
+    <li class="w-fit flex hover:underline transition duration-300 ease-in-out items-center gap-x-1">
+        <i class="fa-solid fa-list-check"></i>
         <a href="retiradas.php">Retiradas</a>
     </li>
     
-    <li class="w-min flex hover:underline hover:text-red-600 transition duration-300 ease-in-out">
+    <li class="w-fit flex hover:underline hover:text-red-600 transition duration-300 ease-in-out items-center gap-x-1">
+        <i class="fa-solid fa-arrow-right-from-bracket"></i>
         <a href="logout.php">Sair</a>
     </li>
 </ul>
