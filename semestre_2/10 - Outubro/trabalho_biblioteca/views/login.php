143f7a1f5033a1219e894efefdd7411949ef3b5c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-07 12:51:09 -0300

    Update: Estilização em login.php

--- semestre_2/10 - Outubro/trabalho_biblioteca/views/login.php
+++ semestre_2/10 - Outubro/trabalho_biblioteca/views/login.php
@@ -6,32 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
 
     <?php include_once('includes/menu.php'); ?>
 
-    <div class="flex flex-col justify-center items-center gap-2">
+    <div class="flex flex-col justify-center items-center gap-2 h-auto">
 
-        <h1 class="font-bold text-4xl text-sky-500 my-4">Login</h1>
+        <h1 class="font-bold text-4xl text-sky-500 my-4">
+            <i class="fa-solid fa-book-open"></i>
+            Login
+        </h1>
 
         <form action="fazerLogin.php" method="POST" class="flex flex-col justify-center items-center gap-2">
 
-            <fieldset class="bg-sky-50 shadow-md p-10 rounded-md w-[40rem] flex flex-col justify-center items-center">
+            <fieldset class="px-10 w-[40rem] flex flex-col justify-center items-center gap-y-8">
+                <!-- <fieldset class="bg-sky-50 shadow-md p-10 rounded-md w-[40rem] flex flex-col justify-center items-center"> -->
 
-                <input type="text" name="login" id="login" placeholder="Login" class="border-[1px] border-slate-600 rounded-md 
-                px-1 py-3 placeholder:pl-2 focus:outline-[#006C4A]">
+                <div>
+                    <label for="login" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Login</label>
+                    <input type="text" name="login" id="login" placeholder="Login" class="rounded-md 
+                border-0 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-500 placeholder:text-zinc-500
+                focus:ring-2 focus:ring-inset focus:ring-sky-500 outline-none text-zinc-800">
+                </div>
 
-                <br></br>
 
-                <input type="password" name="senha" id="senha" placeholder="Senha" class="border-[1px] border-slate-600 rounded-md 
-                px-1 py-3 placeholder:pl-2 focus:outline-[#006C4A]">
 
-                <button type="submit" class="bg-[#006C4A] rounded-full
-                p-2 text-zinc-50 shadow-md hover:shadow-[#A9ACA7] px-4 flex 
-                items-center gap-x-1 transition duration-300 ease-in-out mt-2">
+                <div>
+                    <label for="senha" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Senha</label>
+                    <input type="password" name="senha" id="senha" placeholder="Senha" class="rounded-md 
+                border-0 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-500 placeholder:text-zinc-500 
+                focus:ring-2 focus:ring-inset focus:ring-sky-500 outline-none text-zinc-800">
+                </div>
+                <button type="submit" class="bg-emerald-600 rounded-full py-1 px-24 hover:bg-emerald-800 text-zinc-50">
                     Entrar
                 </button>
             </fieldset>
