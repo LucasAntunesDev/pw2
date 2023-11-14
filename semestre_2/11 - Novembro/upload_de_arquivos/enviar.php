353004683e90e9ac739cdb67a2078e8f4fab6dbf
LucasAntunes06 <lucasantuneslara06@gmail.com>
2023-11-14 14:09:45 -0300

    update: nome geração de aleatório dos arquvios

--- semestre_2/11 - Novembro/upload_de_arquivos/enviar.php
+++ semestre_2/11 - Novembro/upload_de_arquivos/enviar.php
@@ -9,7 +9,11 @@ if ($_FILES['arquivo']['error'])
     echo 'Ocorreu um erro ao fazer o upload do arquivo';
 else {
     $diretorio = 'arquivos/';
-    $nome_completo_arquivo = $diretorio . $_FILES['arquivo']['name'];
+    // $nome_completo_arquivo = $diretorio . $_FILES['arquivo']['name'];
+    $nome_arquivo = uniqid();
+    $extensao_arquivo = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
+    $nome_completo_arquivo = $diretorio . $nome_arquivo. '.' . $extensao_arquivo;
+
     $sucesso = move_uploaded_file($_FILES['arquivo']['tmp_name'], $nome_completo_arquivo);
 
     echo $sucesso ? 'Arquivo salvo com sucesso' : 'Ocorreu um erro ao salvar o arquivo no servidor';
